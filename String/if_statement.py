6b5c2a97f268d9a6c5ef08ac1224d2cd7690b5b6
sairanjith101 <139247459+sairanjith101@users.noreply.github.com>
2023-07-18 10:30:20 +0530

    Add files via upload

---
+++ String/if_statement.py
@@ -0,0 +1,25 @@
+num1 = 100
+num2 = 100
+num3 = 200
+
+# method 1
+if num1 == num2:
+	print ("'num1' equal to 'num2'")
+	
+	if num1 > num3:
+		print ("'num1' greater than 'num3")
+	elif num3 > num2:
+		print ("'num3' greater than 'num2'")
+	else:
+		print ("prints when no statement matches")
+else:
+	print ("'num1' not equal to 'num2'")
+	
+# method 2
+if num1 == num2:
+	print ("'num1' equal to 'num2'")
+	if num1 > num3: print ("'num1' greater than 'num3")
+	elif num3 > num2: print ("'num3' greater than 'num2'")
+	else: print ("prints when no statement matches")
+else: print ("'num1' not equal to 'num2'")
+	
