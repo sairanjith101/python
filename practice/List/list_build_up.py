440a87c3cfc594a4861e10ac6997d15d16fb56f1
sairanjith101 <139247459+sairanjith101@users.noreply.github.com>
2023-07-20 16:44:25 +0530

    Add files via upload

---
+++ practice/List/list_build_up.py
@@ -0,0 +1,9 @@
+# list build up
+names = [] # create a empty list
+names.append('java')  # add an element to the list
+names.append('python')
+names.append('sql')
+names.append('perl')
+
+print (names) # ['java', 'python', 'sql', 'perl']
+print (len(names)) # 4
---
+++ practice/List/list_comprehensions.py
@@ -0,0 +1,14 @@
+# to find the cubical value for elements in the list
+numbers = [1,2,3,4,5]
+cubes = [ num*num*num for num in numbers ]
+print (cubes)
+
+# to change the string to upper case and append a additional string
+languages = ['java','python','perl']
+uppercase = [ langs.upper() + ' language!!!' for langs in languages ]
+print (uppercase)
+
+# adding if statement to narrow down the results based on the requirements
+marks = [65,45,30,90,35,50,70]
+pass_marks = [ m for m in marks if m>=50]
+print (pass_marks)
