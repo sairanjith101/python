f2f933ac5dbae241adc66aa6aa3f36b88da6e100
sairanjith101 <139247459+sairanjith101@users.noreply.github.com>
2023-12-11 18:40:14 +0530

    Add files via upload

---
+++ practice/programize/celsius_to_fahrenheit.py
@@ -0,0 +1,8 @@
+# F = C * (9/5) + 32. 
+
+def fahrenheit():
+    celsius = 40
+    formula = (celsius * (9/5)) + 32
+    return formula
+
+print('40 degree celsious is: ' + str(fahrenheit()) + ' degree faherenheit')
